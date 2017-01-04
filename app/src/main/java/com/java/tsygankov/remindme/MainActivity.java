057043c118efc06a8aef7d2cdf708bd39610647d
Braun2002 <srg.tsygankov@gmail.com>
2017-01-05 00:36:23 +0300

    Create MainActivity and layout

---
+++ app/src/main/java/com/java/tsygankov/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.java.tsygankov.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 05.01.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
